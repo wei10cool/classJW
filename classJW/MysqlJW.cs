de7608ea28bf2e35552e2841bd9fec4333af0847
jw <x215033@yahoo.com.tw>
2020-06-12 09:41:23 +0800

    加入MYSQL讀取方式

--- classJW/MysqlJW.cs
+++ classJW/MysqlJW.cs
@@ -4,13 +4,15 @@ using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using MySql;
+using MySql.Data.MySqlClient;
 
 namespace classJW
 {
-    class MysqlJW
+    public class MysqlJW
     {
-        public class MySqlPars
+        MySqlConnection conn;
+        public class ParameterSP
         {
             /// <summary>
             /// 參數名稱
@@ -25,30 +27,167 @@ namespace classJW
             /// </summary>
             public ParameterDirection praType { get; set; } = ParameterDirection.InputOutput;
         }
-        //public int ExeSql(string sql, List<MySqlPars> pra = null)
-        //{
-        //    int result;
-        //    try
-        //    {
-        //        MySqlCommand cmd = conn.CreateCommand();
-        //        cmd.CommandText = sql;
-        //        foreach (var item in pra)
-        //        {
-        //            cmd.Parameters.Add(new MySqlParameter(item.praName, item.praValue));
-        //        }
-        //        conn.Open();
-        //        result = cmd.ExecuteNonQuery();
-        //        conn.Close();
-        //        return result;
-        //    }
-        //    catch (Exception ex)
-        //    {
-        //        throw ex;
-        //    }
-        //    finally
-        //    {
-        //        conn.Close();
-        //    }
-        //}
+        public MysqlJW(string connectionString){
+            conn = new MySqlConnection(connectionString);
+        }
+        public DataSet getDataSet(string sqlString)
+        {
+            try
+            {
+                conn.Open();
+                DataSet ds = new DataSet();
+                MySqlDataAdapter da = new MySqlDataAdapter(sqlString, conn);
+                da.Fill(ds);
+                return ds;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                conn.Close();
+            }
+
+        }
+        /// <summary>
+        /// 回傳資料DataSet格式_帶參數
+        /// </summary>
+        /// <param name="sqlString">SQL語法</param>
+        /// <param name="parameterSPs">參數清單</param>
+        /// <returns></returns>
+        public DataSet GetDataSet_pra(string sqlString, List<ParameterSP> parameterSPs)
+        {
+            try
+            {
+                conn.Open();
+                MySqlDataAdapter da = new MySqlDataAdapter(sqlString, conn); ;
+                //--Pra
+                //Input 1 Output 2 InputOutput 3 ReturnValue 6
+                foreach (var item in parameterSPs)
+                {
+                    da.SelectCommand.Parameters.Add(new MySqlParameter(item.praName, item.praValue)
+                    { Direction = item.praType });
+                }
+                
+                DataSet ds = new DataSet();
+                da.Fill(ds);
+                da.Dispose();
+                return ds;
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+        /// <summary>
+        /// 執行SQL-只需要知道成功(>=0)或失敗(-1)
+        /// </summary>
+        /// <param name="sqlString">SQL語法</param>
+        /// <returns>int</returns>
+        public int exeSql(string sqlString)
+        {
+            int resultVal= -1;
+            try
+            {
+                MySqlCommand cmd = conn.CreateCommand();
+                cmd.CommandText = sqlString;
+                conn.Open();
+
+                resultVal = cmd.ExecuteNonQuery();
+                conn.Close();
+                return resultVal;
+
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+        /// <summary>
+        ///  執行SQL-只需要知道成功(>=0)或失敗(-1)
+        /// </summary>
+        /// <param name="sqlString">SQL語法</param>
+        /// <param name="parameterSPs">參數</param>
+        /// <returns>int</returns>
+        public int exeSql_pra(string sqlString, List<ParameterSP> parameterSPs)
+        {
+            int resultVal = -1;
+            try
+            {
+                MySqlCommand cmd = conn.CreateCommand();
+                cmd.CommandText = sqlString;
+                //--Pra
+                //Input 1 Output 2 InputOutput 3 ReturnValue 6
+                foreach (var item in parameterSPs)
+                {
+                    cmd.Parameters.Add(new MySqlParameter(item.praName, item.praValue)
+                    { Direction = item.praType });
+                }
+                conn.Open();
+
+                resultVal = cmd.ExecuteNonQuery();
+                conn.Close();
+                return resultVal;
+
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+        /// <summary>
+        /// 執行StoreProcedure
+        /// </summary>
+        /// <param name="procName">參數名稱</param>
+        /// <param name="parameterSPs">參數值</param>
+        /// <returns>DataSet</returns>
+        public DataSet runSP(string procName,List<ParameterSP> parameterSPs)
+        {
+            try
+            {
+                conn.Open();
+
+                MySqlCommand cmd = new MySqlCommand(procName, conn);
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                //--Pra
+                //Input 1 Output 2 InputOutput 3 ReturnValue 6
+                foreach (var item in parameterSPs)
+                {
+                    cmd.Parameters.Add(new MySqlParameter(item.praName, item.praValue)
+                    { Direction = item.praType });
+                }
+                //--
+                MySqlDataAdapter da = new MySqlDataAdapter(cmd);
+                DataSet ds = new DataSet();
+                int result = da.Fill(ds);
+                return ds;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+        
     }
 }
