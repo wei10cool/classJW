fe767c609b12b98d90d863703412420ca9c97831
jw <x215033@yahoo.com.tw>
2021-05-19 13:31:51 +0800

    20210519

--- classJW/Properties/AssemblyInfo.cs
+++ classJW/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // 變更這些屬性的值即可修改組件的相關
 // 資訊。
 [assembly: AssemblyTitle("classJW")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("oracle,mssql,mysql,postgre,http")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("classJW")]
-[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
